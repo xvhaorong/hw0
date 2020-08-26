00f0dc4d879cfde463fa7014467d586a7138a721
xvhaorong <hxx5086@psu.edu>
2020-08-26 19:38:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#print ("Hello World!")
+name = input ("Enter temperature in celsius: ")
+
+
+
+age = int(name)*1.8 + 32 
+#print(age + 1)
+
+print(name + "°" + "in Celsius is equivalent to" + str(age)+ "°" " Fahrenheit." )
+
+#print(f"I am {age+1} next year")
